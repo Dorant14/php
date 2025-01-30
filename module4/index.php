94583b3090681f94625029171ff29eb43b8f7e10
Dorant14 <dorant.hjadri12@gmail.com>
2025-01-30 16:14:58 +0100

    rrrrr

---
+++ module4/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    for($numri = 50; $numri <=100; $numri+=2){
+        echo"$numri .<br>";
+    }
+
+    $studens = ["Dorant","Bled","Nil","Blin"];
+    foreach($studens as $studens){
+        echo $studens. "<br>";
+    }
+
+    
+?>
