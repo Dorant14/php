9be0c2a3fa2c080c499a1cce50d71f36e658cfb9
Dorant14 <dorant.hajdari12@gmail.com>
2025-02-04 15:29:12 +0100

    aaa

--- module4/index.php
+++ module4/index.php
@@ -9,5 +9,15 @@
         echo $studens. "<br>";
     }
 
+    $nr1=3
+    $nr2=5
+    function numbers($nr1, $nr2){
+        if($nr1>$nr2){
+            echo($nr1)
+        }
+        if($nr1<$nr2){
+            echo($nr2)
+        }
+    }
     
 ?>
