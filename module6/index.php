d6079a080ef21bbdc3b91420067c201f029aaca3
Dorant14 <dorant.hajdari12@gmail.com>
2025-02-18 15:58:26 +0100

    qqq

--- module6/index.php
+++ module6/index.php
@@ -1,5 +1,29 @@
-<? 
+<?php 
 
-    
+//fopen
+  $my_file = fopen("file1.text","w");
+  
+  ///content
+
+fclose($my_file);
+
+$filename = "ds.txt";
+
+$file = fopen($filename,"r");
+
+$size= filesize($filename);
+
+$my_filedata = fread($file,$size );
+echo $my_filedata . "<br>"
+
+fclose($file);
+
+$my_file =fopen("exampel.txt", "r");
+
+while(!feof($my_file)) {
+    echo fgets($my_file) . "<br>";
+}
+
+$file = fopen()
 
 ?>
