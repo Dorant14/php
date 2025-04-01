314e6c32c1f45d832858803e1f871d50c148a581
Dorant14 <dorant.hajdari12@gmail.com>
2025-04-01 16:10:49 +0200

    module14

--- module13/config.php
+++ module13/config.php
@@ -1,4 +1,5 @@
 <?php
+       session_start();
     $server = 'localhost';
     $username = 'root';
     $dbname = 'db';
@@ -7,6 +8,6 @@
     try{
         $connection = new PDO("mysql:host=$server;dbname=$dbname;", $username, $password);
     } catch(Exception $e){
-        echo "Somthing went wrong";
+        echo "Somthing went wrong"; . e->getMessage();
     }
 ?>
