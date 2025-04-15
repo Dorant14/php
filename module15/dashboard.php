7f08896b2d71bfd7fef1bd51838c0cb03646ef6e
Dorant14 <dorant.hajdari12@gmail.com>
2025-04-15 07:10:07 -0700

    module15

---
+++ module15/dashboard.php
@@ -0,0 +1,87 @@
+<?php
+    include_once('config.php');
+    $sql = 'SELECT * FROM produtcs';
+    $selectProducts = $conn->prepare($sql);
+    $selectProducts->execute();
+
+    $produtcs_data=$selectProducts->fetchAll();
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,td,th {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        td,th {
+            padding: 10px 20px;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+        <thead>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Quantity</th>
+            <th>Price</th>
+    </thead>
+    <tbody>
+        
+        <?php 
+            foreach($produtcs_data as $product){
+        ?>
+        <tr>
+            <td><?= $produtct['id']?></td>
+            <td><?= $produtct['title']?></td>
+            <td><?= $produtct['description']?></td>
+            <td><?= $produtct['quanity']?></td>
+            <td><?= $produtct['price']?></td>
+            <td><?= "<a herf ='updateProduct.php?id=$product['id']'>Update</a>"?></td>
+            <td><?= "<a herf ='deleteProduct.php?id=$product['id']'>Delete</a>"?></td>
+        </tr>
+
+    <tbody>
+    </table>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a href="#" class="navbar-brand col-sm-3 col-md-2n mr-0">Welcome, <i><?php echo $_SESSION['username
+        ']?></i> </a>
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="logout.php" href="nav-link">Sign out</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky">
+                  <li><a href=""></a></li>
+                  <li><a href=""></a></li>
+    </div>
+    <?php 
+    include_once('config.php');
+    $sql = 'SELECT * FROM user';
+    $getUsers = $conn->prepare($sql);
+    $getUsers ->execute();
+    $users = $getUsers->fetchAll();
+    ?>
+
+    <a href="index.php">Add User</a> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
